6397f5cdf27191054c0f8c9d3d689b6c5a668c87
Angela Sun <angelasun1995@gmail.com>
2022-03-16 20:32:23 -0400

    Add buttons via JS

--- script.js
+++ script.js
@@ -1,13 +1,37 @@
-const calculator = {
-  add: (a, b) => a + b,
-  subtract: (a, b) => a - b,
-  multiply: (a, b) => a * b,
-  divide: (a, b) => a / b,
+const html = {
+  body: document.querySelector("body"),
+  calculatorBody: document.querySelector(".calculator"),
+  buttonContainer: document.querySelector(".buttons"),
+  display: document.querySelector(".display"),
 };
-function operate(operator, a, b) {
-  return operator(a, b);
-}
 
-function initCalc() {}
+const calculator = {
+  add: { fn: (a, b) => a + b, symbol: "+" },
+  subtract: { fn: (a, b) => a - b, symbol: "-" },
+  multiply: { fn: (a, b) => a * b, symbol: "x" },
+  divide: { fn: (a, b) => a / b, symbol: "%" },
+  operate: { fn: (operator, a, b) => operator(a, b), symbol: "=" },
+};
+
+function initCalc() {
+  html.display.textContent = "1233";
+
+  const clear = document.createElement("div");
+  clear.classList.add("clear");
+  html.buttonContainer.appendChild(clear);
+  for (let i = 0; i < 10; i++) {
+    const button = document.createElement("div");
+    button.classList.add(`number`, `number-${i}`);
+    button.textContent = i;
+    html.buttonContainer.appendChild(button);
+  }
+
+  for (key in calculator) {
+    const fnButton = document.createElement("div");
+    fnButton.classList.add(`function`, `${key}`);
+    fnButton.textContent = calculator[key].symbol;
+    html.buttonContainer.appendChild(fnButton);
+  }
+}
 
 initCalc();
