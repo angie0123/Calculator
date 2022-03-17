bb08c4992a127e5b4c807ac2a0d07b6b9012b58b
Angela Sun <angelasun1995@gmail.com>
2022-03-16 20:08:43 -0400

    Refactor calculation functions into single object, set up global html container

--- script.js
+++ script.js
@@ -1,19 +1,13 @@
-function add(a, b) {
-  return a + b;
-}
-
-function subtract(a, b) {
-  return a - b;
-}
-
-function multiply(a, b) {
-  return a * b;
-}
-
-function divide(a, b) {
-  return a / b;
-}
-
+const calculator = {
+  add: (a, b) => a + b,
+  subtract: (a, b) => a - b,
+  multiply: (a, b) => a * b,
+  divide: (a, b) => a / b,
+};
 function operate(operator, a, b) {
   return operator(a, b);
 }
+
+function initCalc() {}
+
+initCalc();
