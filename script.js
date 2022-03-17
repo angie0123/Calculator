2b9ad559eddebed21f2e3aab9af05a8232334472
Angela Sun <angelasun1995@gmail.com>
2022-03-16 21:14:25 -0400

    Add styling

--- script.js
+++ script.js
@@ -21,14 +21,18 @@ function initCalc() {
   html.buttonContainer.appendChild(clear);
   for (let i = 0; i < 10; i++) {
     const button = document.createElement("div");
-    button.classList.add(`number`, `number-${i}`);
+    button.classList.add(`button`, `number`, `number-${i}`);
     button.textContent = i;
     html.buttonContainer.appendChild(button);
   }
+  const decimalBtn = document.createElement("div");
+  decimalBtn.classList.add("button", "decimal");
+  decimalBtn.textContent = ".";
+  html.buttonContainer.appendChild(decimalBtn);
 
   for (key in calculator) {
     const fnButton = document.createElement("div");
-    fnButton.classList.add(`function`, `${key}`);
+    fnButton.classList.add(`button`, `function`, `${key}`);
     fnButton.textContent = calculator[key].symbol;
     html.buttonContainer.appendChild(fnButton);
   }
